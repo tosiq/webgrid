15ccf3c0b59a6dd0e614853e6f9caf985b14822c
tosiq ahmed <m.tosiqshafiq@yahoo.com>
2021-04-18 03:09:09 +0500

    work is done

---
+++ WebApplication9/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication9.Models;
+
+namespace WebApplication9.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        imtiazEntities db = new imtiazEntities();
+       static List<login> db1 = new List<login>();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(login o)
+        {
+            db.logins.Add(o);
+            db.SaveChanges();
+            db1.Add(o);
+            ModelState.Clear();
+
+            return RedirectToAction("form","Home");
+        }
+
+        public ActionResult form()
+        {
+            return View(db1.ToList());
+        }
+
+
+    }
+}
